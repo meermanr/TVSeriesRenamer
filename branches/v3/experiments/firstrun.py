5c22d28c9ae9a8d197cf917e009becc6e3e24844
meermanr <meermanr@0ba0757d-5d28-0410-82a9-afbb3cb19212>
2008-02-29 23:27:08 +0000

    Adding comments and an optimisation (don't bother to fiddle the timestamps if it is not the first-run)
    git-svn-id: file:///store/svn/tvrenamer@67 0ba0757d-5d28-0410-82a9-afbb3cb19212

--- branches/v3/experiments/firstrun.py
+++ branches/v3/experiments/firstrun.py
@@ -3,6 +3,12 @@
 This is a proof-of-concept script that attempts to determine if it is being run
 for the first time based purely on the timestamps contained in the filesystem
 against the script's container.
+
+The last-accessed timestamp is not used because people often disable updating
+this field to speed up file-access (because when it is enabled every read of a
+file incurs a write-back to update the metadata)
+
+Note that this has been tested on Linux, and works through symlinks.
 """
 
 import sys, os
@@ -16,14 +22,16 @@ if sys.argv[0] is not '':
 
 	if stat_result.st_mtime == stat_result.st_ctime:
 		print "\nTherefore: First run!\n"
+
+		os.chmod( sys.argv[0], stat_result.st_mode )
+
+		stat_result = os.stat( sys.argv[0] )
+		print "After:"
+		print "  Access: 0x%x" % stat_result.st_atime
+		print "  Modify: 0x%x" % stat_result.st_mtime
+		print "  Change: 0x%x" % stat_result.st_ctime
+
 	else:
 		print "\nTherefore: Not first run.\n"
 
-	os.chmod( sys.argv[0], stat_result.st_mode )
-
-	stat_result = os.stat( sys.argv[0] )
-	print "After:"
-	print "  Access: 0x%x" % stat_result.st_atime
-	print "  Modify: 0x%x" % stat_result.st_mtime
-	print "  Change: 0x%x" % stat_result.st_ctime
 
