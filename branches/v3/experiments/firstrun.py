a121c9aeb154d5ef3e39cbfa67e5791663874f6b
meermanr <meermanr@0ba0757d-5d28-0410-82a9-afbb3cb19212>
2008-03-01 01:00:46 +0000

    Working in all 3 OSs
    git-svn-id: file:///store/svn/tvrenamer@69 0ba0757d-5d28-0410-82a9-afbb3cb19212

--- branches/v3/experiments/firstrun.py
+++ branches/v3/experiments/firstrun.py
@@ -4,30 +4,50 @@ This is a proof-of-concept script that attempts to determine if it is being run
 for the first time based purely on the timestamps contained in the filesystem
 against the script's container.
 
-The last-accessed timestamp is not used because people often disable updating
-this field to speed up file-access (because when it is enabled every read of a
-file incurs a write-back to update the metadata)
+Linux and Windows treat file timestamps differently enough that we must account
+for each seperately. In Linux st_ctime is a timestamp for the last change to a
+file (either modifying file contents, or file metadata), in Windows it is
+"creation time".
 
-Note that this has been tested on Linux and OS X, and works through symlinks.
+Therefore when the file permissions are updated via chmod(), the "last changed"
+time is updated in Linux, but no times are updated in Windows. When a file is
+modifed by truncate() both "last changed" and "last modified" are updated in
+Linux but only "last modified" is updated in Windows.
 
-Didn't work on Windows Vista...
+It follows that our solution is to modify the file (updating "changed" and
+"modified x2") and then update its metadata (updating "changed" only). We
+modify the file by truncating it to the size it already is (no data loss) and
+we update metadata by setting the permissions to what they currenly are (again,
+no change).
+
+Tested on Linux, OS X and Windows Vista.
 """
 
-import sys, os
+from time	import sleep
+from sys	import argv
+from os		import stat, chmod
 
-if sys.argv[0] is not '':
-	stat_result = os.stat( sys.argv[0] )
+if argv[0] is not '':
+	stat_result = stat( argv[0] )
 	print "Before:"
 	print "  Access: 0x%x" % stat_result.st_atime
 	print "  Modify: 0x%x" % stat_result.st_mtime
 	print "  Change: 0x%x" % stat_result.st_ctime
 
-	if stat_result.st_mtime == stat_result.st_ctime:
+	#if stat_result.st_mtime == stat_result.st_ctime:
+	if abs( stat_result.st_mtime - stat_result.st_ctime) < 1:
 		print "\nTherefore: First run!\n"
 
-		os.chmod( sys.argv[0], stat_result.st_mode )
+		f = file( argv[0], "a" )
+		f.seek(0, 2)	# 0 bytes from "end of file" (=2)
+		f.truncate()	# To current position: no change
+		f.close()
 
-		stat_result = os.stat( sys.argv[0] )
+		sleep(1)	# Required to make sure the timestamps will be different
+
+		chmod( argv[0], stat_result.st_mode)	# Update permissions to current permissions: no change
+
+		stat_result = stat( argv[0] )
 		print "After:"
 		print "  Access: 0x%x" % stat_result.st_atime
 		print "  Modify: 0x%x" % stat_result.st_mtime
