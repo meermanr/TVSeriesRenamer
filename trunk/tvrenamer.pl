00ea8a7ddf5dfb62224f79958658bfe84cae2e4d
meermanr <meermanr@0ba0757d-5d28-0410-82a9-afbb3cb19212>
2008-06-06 22:47:37 +0000

    Released as v2.35
    git-svn-id: file:///store/svn/tvrenamer@120 0ba0757d-5d28-0410-82a9-afbb3cb19212

--- trunk/tvrenamer.pl
+++ trunk/tvrenamer.pl
@@ -151,7 +151,8 @@ else{
 	($series, $season) = ($series =~ /(.+?)(?:\s+(\d+)x)?$/i);  # Extract season number (NB Minimal "+?" and non-capturing parenthesis)
 }
 #------------------------------------------------------------------------------}}}
-my $version = "TV Series Renamer 2.34
+my $version = "TV Series Renamer 2.35
+
 \nReleased 22 March 2008\n"; # {{{
 my $helpMessage = 
 "Usage: $0 [OPTIONS] [FILE|URL|-]
