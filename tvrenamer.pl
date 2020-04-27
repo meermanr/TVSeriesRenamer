32ee97f0acaf52073bfc6072fe397e504dfbc0eb
Robert Meerman <robert.meerman@gmail.com>
2020-04-27 16:36:37 +0100

    Release v2.57

--- tvrenamer.pl
+++ tvrenamer.pl
@@ -154,7 +154,7 @@ my $implicit_format = 1;  # 1="Soft" format, use internal algorithm to detect in
 my $do_win32_associate = 0;	# 0=Do nothing, 1=associate, -1=unassociate
 
 #------------------------------------------------------------------------------}}}
-my $version = "TV Series Renamer v2.56\nReleased 24 April 2012\n"; # {{{
+my $version = "TV Series Renamer v2.57\nReleased 27 April 2020\n"; # {{{
 print $version;
 my $helpMessage =
 "Usage: $0 [OPTIONS] [FILE|URL|-]
